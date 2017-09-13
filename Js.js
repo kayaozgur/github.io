1b12062d6c6fb9b99fa8e14dda59507c7ca97ec9
kayaozgur <okaya.gm@gmail.com>
2017-09-13 19:54:48 +0300

    js eklendi

---
+++ Js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#btnsarı").click(function(){
+       $("p").css({"background-color": "yellow", "font-size": "200%"});
+    });
+
+    $("#btnmavi").click(function(){
+       $("p").css({"background-color": "blue", "font-size": "200%"});
+    });
+});
